654b0e4a89bb3e23becd7477754d59e245ab6bdc
Dan Milne <d@nmilne.com>
2015-08-26 13:02:06 +1000

    Move the configuration class into the configuration file.  This lets us set up configuration prior to starting sinatra.

--- lib/ferver.rb
+++ lib/ferver.rb
@@ -6,24 +6,3 @@ require_relative 'ferver/file_id_request'
 require_relative 'ferver/file_list'
 require_relative 'ferver/found_file'
 require_relative 'ferver/version'
-
-module Ferver
-  # By default, serve files from current location when the gem is called.
-  DEFAULT_FILE_SERVER_DIR_PATH = './'
-
-  class << self
-      attr_accessor :configuration
-  end
-
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
-
-  def self.reset
-    @configuration = Configuration.new
-  end
-
-  def self.configure
-    yield(configuration)
-  end
-end
--- lib/ferver/configuration.rb
+++ lib/ferver/configuration.rb
@@ -1,4 +1,23 @@
 module Ferver
+  # By default, serve files from current location when the gem is called.
+  DEFAULT_FILE_SERVER_DIR_PATH = './'
+
+  class << self
+      attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.reset
+    @configuration = Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
   class Configuration
     attr_accessor :directory_path
 
